ddcacd2c2949756c0fa79b571b8a0bd7c6ba3504
umitao <umitao@gmail.com>
2020-05-15 19:13:48 +0300

    B Tracing, exercise 2 completed

--- InClass/B-Tracing code/exercise2/exercise2.js
+++ InClass/B-Tracing code/exercise2/exercise2.js
@@ -1,16 +1,18 @@
 function removeVowels(word) {
   var characters = word.split("");
-
+  var vowels = ["a", "e", "i", "o", "u"];
   var result = [];
 
-  characters.forEach(function(character) {
-    if (
-      character === "a" ||
-      character === "o" ||
-      character === "i" ||
-      character === "e" ||
-      character === "u"
-    ) {
+  characters.forEach(function (character) {
+    // Redundancy in if conditions, fixed with vowels array and includes method.
+    // if (
+    //   character === "a" ||
+    //   character === "o" ||
+    //   character === "i" ||
+    //   character === "e" ||
+    //   character === "u"
+    // )
+    if (!vowels.includes(character)) {
       result.push(character);
     } else {
       result.push("_");
@@ -21,7 +23,7 @@ function removeVowels(word) {
 }
 
 function removeVowelsForWords(words) {
-  var result = words.map(function(word) {
+  var result = words.map(function (word) {
     return removeVowels(word);
   });
 
@@ -32,13 +34,14 @@ function removeVowelsForWords(words) {
   Task 1
   Let's trace this piece of code - what is the value of result with this input
   */
-  var result = removeVowels('samuel');
-
-
+var result = removeVowels("samuel");
+console.log(result); //Returns _a_ue_ Only removes consonants. Not vowels
 
 /*
 	Task 2
 	Trace what happens if we call the function removeVowelsForWords with this input: ["Irina", "Etza", "Daniel"]
     expected output?:
 */
-
+var wordArray = ["Irina", "Etza", "Daniel"];
+var arrayWords = removeVowelsForWords(wordArray);
+console.log(arrayWords);
